6f900eab57744d8b15a91bef95be3563daed86eb
Karthikeyan D <81141000+karthikeyanreactdev@users.noreply.github.com>
2023-04-08 17:15:45 +0530

    db updated

--- db.js
+++ db.js
@@ -6,11 +6,11 @@ const Pool = require("pg").Pool;
 const pool = new Pool({
   user: "postgres",
   // password: "SMBS123$",
-  password: "1234",
+  password: "12345678",
   host: "database-1.c8pok7r318td.eu-north-1.rds.amazonaws.com",
   port: 5432,
   //database: "sample"
-  database: "acquirehive",
+  database: "AcquireHive",
 });
 
 module.exports = pool;
